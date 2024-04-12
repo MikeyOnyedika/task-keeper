8a1c91daabd801eb81014c2c65731e8e4150ae84
MikeyOnyedika <michaelvik520@gmail.com>
2024-04-12 17:04:43 +0100

    redirect from login or signup page if user session is still active

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,20 @@
 import { ReactNode } from "react";
 import { Logo } from "@/app/assets";
 import Image from "next/image";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
+  // try to see if there's a valid session
+  const session = await getServerSession();
+  if (session) {
+    redirect("/projects");
+  }
+
   return (
     <main>
       <header className="py-2 px-4">
-        <Image src={Logo} alt=""/>
+        <Image src={Logo} alt="" />
       </header>
       {children}
     </main>
